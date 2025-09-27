bd12b2514ce362903296c12e9198eef2b0ef5257
César Freitas <costacesar014@gmail.com>
2025-09-26 22:56:10 -0300

    sucesso

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, render_template, redirect, request, flash
+import json
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'DOM123'
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+
+    nome = request.form.get('nome')
+    senha = request.form.get('senha')
+
+    with open('faculdade/html_em_python/usuarios.json') as usuario_temporaria:
+        usuarios = json.load(usuario_temporaria)
+        cont = 0    
+        for usuario in usuarios:
+            cont += 1
+            if usuario["nome"] == nome and usuario["senha"] == senha:
+                return render_template('usuario.html')
+            
+            if cont >= len(usuarios):
+                flash('USUARIO INVALIDO')
+                return redirect('/')
+
+    print(usuarios)
+    return "Login processado!"
+
+
+if __name__ in "__main__":
+    app.run(debug=True)
